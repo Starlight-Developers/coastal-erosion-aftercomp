97ad9a12882fd49580a3ac434af9747652b9ee58
AxizY <24615015+AxizY@users.noreply.github.com>
2022-06-19 00:19:13 -0400

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -2,12 +2,18 @@ const canvas = document.getElementById("main");
 
 const ctx = canvas.getContext('2d');
 
+
 var map = new Map();
+var sea = new Sea(.3);
 
 map.createMap();
+sea.create();
 map.addPixel(60, 60, "water");
+
 
 window.setInterval(function() {
     map.moveMap();
+    sea.move();
+    sea.draw();
     map.drawMap();
 }, 100)
