d3edf373999ae139ee050809a90fde3ef518e634
AxizY <24615015+AxizY@users.noreply.github.com>
2022-06-19 00:19:30 -0400

    sea

--- grid.js
+++ grid.js
@@ -2,13 +2,24 @@ function randColor() {
     return Math.floor(Math.random() * 256);
 }
 
+function colorLerp(t, color1, color2) {
+    var color3 = {r:0, g:0, b:0};
+    color3.r = color1.r + (color2.r - color1.r) * t;
+    color3.g = color1.g + (color2.g - color1.g) * t;
+    color3.b = color1.b + (color2.b - color1.b) * t;
+    return color3;
+}
+
 class Pixel {
     constructor(xpos, ypos, type) {
         this.type = type;
         if (type == "sand") {
-            this.color = 'rgba(209, 167, 17, '+(1-Math.random()/2)+')';
+            var color = colorLerp((Math.random()*.6), {r:209, g:167, b:17}, {r:255, g:255, b:255});
+            this.color = 'rgba('+(color.r)+', '+(color.g)+', '+(color.b)+', 1.0)';
         } else if (type == "water") {
-            this.color = 'rgba(38, 194, 222, 1.0)';
+            this.color = 'white';
+        } else if (type == "rand") {
+            this.color = 'black';
         }
         this.x = xpos;
         this.y = ypos;
@@ -20,8 +31,9 @@ class Pixel {
     }
 
     move() {
-        // this.x -= 1;
-        this.y += 1;
+        if (this.type == "water") {
+            this.y -= 1;
+        }
     }
 }
 
@@ -35,15 +47,16 @@ class Map {
         for (var i = 0; i < this.x.length; i++) {
             this.x[i] = new Array(80);
             for (var b = 0; b < this.x.length; b++) {
-                if (i+b<40) {
-                    this.addPixel(i, b, "sand");
+                if (b<15) {
+                    if (!(b == 14 && Math.random()*b>8)) {
+                        this.addPixel(i, b, "sand");
+                    }
                 }
             }
         }
     }
 
     drawMap() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
         this.pixels.forEach(pixel => {
             pixel.draw();
         })
@@ -51,8 +64,8 @@ class Map {
 
     moveMap() {
         this.pixels.forEach(pixel => {
-            if (pixel.y != 79) {
-                if (this.x[pixel.x][pixel.y+1] == null) {
+            if (pixel.y != 0) {
+                if (this.x[pixel.x][pixel.y-1] == null) {
                     this.x[pixel.x][pixel.y] = null;
                     pixel.move();
                     this.x[pixel.x][pixel.y] = pixel;
@@ -65,5 +78,53 @@ class Map {
         var pix = new Pixel(x, y, type)
         this.pixels.push(pix);
         this.x[x][y] = pix;
+    }
+}
+
+function distance(x, y, pixel) {
+    return Math.sqrt((pixel.x-x)**2+(pixel.y-y)**2)
+}
+
+class Sea {
+    constructor(a) {
+        this.a = a;
+    }
+
+    create() {
+        this.pixels = [];
+        for (var i = 0; i < 50; i++) {
+            this.pixels.push(new Pixel(Math.random()*81, Math.random()*81, "rand"))
+            this.pixels[this.pixels.length-1].draw();
+        }
+    }
+
+    draw() {
+        for (var x = 0; x < 80; x++) {
+            for (var b = 0; b < 80; b++) {
+                var shortDist = 114;
+                this.pixels.forEach(pix => {
+                    if (distance(x, b, pix) < shortDist) {
+                        shortDist = distance(x, b, pix);
+                    }
+                })
+                var g = ((shortDist*40))/255;
+                g = Math.round(g*this.a)/this.a;
+                if (g != 0) g = 1;
+                var water = colorLerp(g, {r: 42, g: 184, b:245}, {r:24, g:107, b:143});
+                ctx.fillStyle = 'rgba('+water.r+','+water.g+','+water.b+', 1.0)';
+                ctx.fillRect(x*10, b*10, 10, 10);
+            }
+        }
+    }
+
+    move() {
+        this.pixels.forEach(pix => {
+            var nX = Math.round(Math.random()*2)-1;
+            var nY = Math.round(Math.random()*2)-1;
+            if(pix.x+nX < 80 && pix.x+nX>0 && pix.y+nY < 80 && pix.y+nY>0) {
+                pix.x += nX;
+                pix.y += nY;
+            }
+        })
     }
 }
