2c77ee8f17d7404055e60d1cf24deb3d27e34c55
AxizY <24615015+AxizY@users.noreply.github.com>
2022-06-19 22:58:07 -0400

    Update grid.js

--- grid.js
+++ grid.js
@@ -16,11 +16,20 @@ class Pixel {
         if (type == "sand") {
             var color = colorLerp((Math.random()*.6), {r:209, g:167, b:17}, {r:255, g:255, b:255});
             this.color = 'rgba('+(color.r)+', '+(color.g)+', '+(color.b)+', 1.0)';
+            this.plant = false;
         } else if (type == "water") {
-            this.color = 'white';
+            this.shore = Math.floor(Math.random()*3)+3;
+            this.removed = false;
+            this.color = 'rgba(255, 255, 255, 0.8)';
         } else if (type == "rand") {
             this.color = 'black';
+        } else if (type == "wall") {
+            this.color = 'black';
+            this.health = 20;
+        } else if (type == "plant") {
+            this.color = 'green';
         }
+        
         this.x = xpos;
         this.y = ypos;
     }
@@ -62,22 +71,70 @@ class Map {
         })
     }
 
+    erode(pixel) {
+        var sand = this.getPixel(pixel.x, pixel.y, "sand").pixel;
+        var max = .1;
+        if(sand.plant == true) {
+            max = .01;
+        }
+        if (Math.random() < max && pixel.removed == false && sand != null  && this.getPixel(pixel.x, pixel.y+1, "sand").pixel == null) {
+            pixel.removed = true;
+            this.pixels.splice(this.getPixel(pixel.x, pixel.y, "sand").index, 1);
+            pixel.color = 'red';
+        }
+        pixel.y += 1;
+    }
+
     moveMap() {
         this.pixels.forEach(pixel => {
-            if (pixel.y != 0) {
-                if (this.x[pixel.x][pixel.y-1] == null) {
-                    this.x[pixel.x][pixel.y] = null;
+            if (pixel.y != 0 && pixel.type == "water") {
+                if (pixel.shore == 0) {
+                    if (this.getPixel(pixel.x, pixel.y, "sand").pixel != null) {
+                        this.erode(pixel);
+                    } else {
+                        this.pixels.splice(this.getPixel(pixel.x, pixel.y, "water").index, 1);
+                    }
+                } else if (this.getPixel(pixel.x, pixel.y-1, "sand").pixel == null) {
+                    if (this.getPixel(pixel.x, pixel.y, "sand").pixel == null) {
+                        var w = this.getPixel(pixel.x, pixel.y-1, "wall");
+                        if (w.pixel == null) {
+                            pixel.move();
+                        } else {
+                            pixel.shore = 0;
+                            w.pixel.health -= Math.floor(Math.random()*3)+1;
+                            if (w.pixel.health <= 0) {
+                                this.pixels.splice(w.index, 1);
+                            }
+                        }
+                    } else {
+                        pixel.shore = 0
+                        this.erode(pixel);
+                    }
+                } else if (pixel.shore > 0) {
                     pixel.move();
-                    this.x[pixel.x][pixel.y] = pixel;
+                    pixel.shore -= 1;
                 }
             }
+            if(pixel.y == 0 && pixel.type == "water") {
+                this.pixels.splice(this.getPixel(pixel.x, pixel.y, "water").index, 1);
+            }
         })
+    }
+
+    getPixel(x, y, type) {
+        var p, i;
+        this.pixels.forEach(function(pix, index) {
+            if (pix.x == x && pix.y == y && pix.type == type) {
+                p = pix;
+                i = index;
+            }
+        })
+        return {pixel:p, index:i};
     }
 
     addPixel(x, y, type) {
         var pix = new Pixel(x, y, type)
         this.pixels.push(pix);
-        this.x[x][y] = pix;
     }
 }
 
